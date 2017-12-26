d5280178eec84b952dcd43a1e5e2909ff64a5855
dhruv-karan <dhruvkaran24@gmail.com>
2017-12-26 10:47:05 -0800

    setup start script and horu

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs =require('hbs');
   const fs =require('fs');
+   const port=process.env.PORT ||3000;
 
 var app = express();
  hbs.registerPartials(__dirname + '/views/footer');
@@ -61,6 +62,6 @@ app.get('/bad',(req,res)=>{
   })
 })
 
-app.listen(3000, ()=>{
-  console.log('server kicjed off');
+app.listen(port, ()=>{
+  console.log(`server started at${port}`);
 } );
